106af7d2fa1fb0b02a7f6763736df4803ebb7dc2
Philip Taron <philip.taron@gmail.com>
2024-07-08 12:58:19 -0700

    nixpkgs-check: extract to package and passthru every other Nix and Lix version
    To run against a specific Nix or Lix version, run this:
    
    ```
    nix-build -A nixpkgsCheck.nixVersions.nix_2_20
    nix-build -A nixpkgsCheck.lixVersions.lix_2_90
    ```
    
    To run against all Nix or Lix versions, run this:
    
    ```
    nix-build -A nixpkgsCheck.nixVersions
    nix-build -A nixpkgsCheck.lixVersions
    ```
    
    CI will build the following versions:
    
    * `nixpkgsCheck` (using `nixVersions.stable`)
    * `nixpkgsCheckWithMinimumNix` (using `nixVersions.minimum`)
    * `nixpkgsCheckWithLatestNix` (using `nixVersions.latest`)
    * `nixpkgsCheckWithGitNix` (using `nixVersions.git`)
    * `nixpkgsCheckWithStableLix` (using `lixVersions.stable`)
    * `nixpkgsCheckWithLatestLix` (using `lixVersions.latest`)
    
    These new tests exposes that Nix versions greater than 2.20 fail.
    
    We don't currently have a derivation for unwrapped `nixpkgs-check-by-name`. Maybe soon.

---
+++ nixpkgs-check.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  runCommand,
+  nixpkgs-check-by-name,
+  initNix,
+  nixpkgs,
+  nix,
+  nixVersions,
+  lixVersions,
+}:
+let
+  # Given an attrset, return only the values that both eval and are derivations.
+  #
+  # We do this to avoid encoding information about which names are in present in the attrset.
+  # For instance, `nixVersions` contains `nix_2_10`, which throws, and `lixVersions` does not
+  # contain `minimum` or `git`, but `nixVersions` does.
+  #
+  # Let's just map over those attrsets and return what's useful from there.
+  derivationsFromAttrset =
+    attrset:
+    lib.filterAttrs (
+      name: value:
+      let
+        eval = builtins.tryEval value;
+      in
+      eval.success && lib.isDerivation eval.value
+    ) attrset;
+
+  mkNixpkgsCheck =
+    name: nix:
+    runCommand "test-nixpkgs-check-by-name-with-${nix.name}"
+      {
+        nativeBuildInputs = [
+          nixpkgs-check-by-name
+          nix
+        ];
+
+        passthru = {
+          # Allow running against all other Nix versions.
+          nixVersions = lib.mapAttrs mkNixpkgsCheck (derivationsFromAttrset nixVersions);
+
+          # Allow running against all other Lix versions.
+          lixVersions = lib.mapAttrs mkNixpkgsCheck (derivationsFromAttrset lixVersions);
+        };
+      }
+      ''
+        ${initNix}
+        # This is what nixpkgs-check-by-name uses
+        export NIX_CHECK_BY_NAME_NIX_PACKAGE=${lib.getBin nix}
+        ${nixpkgs-check-by-name}/bin/.nixpkgs-check-by-name-wrapped --base "${nixpkgs}" "${nixpkgs}"
+        touch $out
+      '';
+in
+mkNixpkgsCheck nix.name nix
