d551581649c5029a270a15a5a6a67b6ba23a1c03
Silvan Mosberger <contact@infinisil.com>
2024-07-19 22:25:37 +0200

    Set up for testing multiple Nix versions
    While the parent commit makes it use a fixed Nix version,
    we should also run test for other Nix versions.

--- package.nix
+++ package.nix
@@ -2,8 +2,14 @@
   lib,
   rustPlatform,
   nix,
+  nixVersions,
   clippy,
   makeWrapper,
+
+  nixVersionsToTest ? [
+    nix
+    nixVersions.stable
+  ],
 
   nixpkgsLibPath,
   initNix,
@@ -28,15 +34,28 @@ rustPlatform.buildRustPackage {
   };
   cargoLock.lockFile = ./Cargo.lock;
   nativeBuildInputs = [
-    nix
     clippy
     makeWrapper
   ];
   env.NIX_CHECK_BY_NAME_EXPR_PATH = "${runtimeExprPath}";
   env.NIX_CHECK_BY_NAME_NIX_PACKAGE = lib.getBin nix;
   env.NIX_PATH = "test-nixpkgs=${testNixpkgsPath}:test-nixpkgs/lib=${nixpkgsLibPath}";
-  preCheck = initNix;
-  postCheck = ''
+  checkPhase = ''
+    # This path will be symlinked to the current version that is being tested
+    nixPackage=$(mktemp -d)/nix
+    # For initNix
+    export PATH=$nixPackage/bin:$PATH
+    # This is what nixpkgs-check-by-name uses
+    export NIX_CHECK_BY_NAME_NIX_PACKAGE=$nixPackage
+
+    ${lib.concatMapStringsSep "\n" (nix: ''
+      ln -s ${lib.getBin nix} "$nixPackage"
+      echo "Testing with $(nix --version)"
+      ${initNix}
+      runHook cargoCheckHook
+      rm "$nixPackage"
+    '') (lib.unique nixVersionsToTest)}
+
     # --tests or --all-targets include tests for linting
     cargo clippy --all-targets -- -D warnings
   '';
