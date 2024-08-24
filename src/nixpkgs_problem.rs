837873e9f7cec0b64aa3c3a8289b119e7a4e85c4
Silvan Mosberger <contact@infinisil.com>
2024-08-24 22:25:32 +0200

    Improve error message for paths pointing outside package dir

--- src/nixpkgs_problem.rs
+++ src/nixpkgs_problem.rs
@@ -361,9 +361,17 @@ impl fmt::Display for NixpkgsProblem {
                             "{relative_package_dir}: File {subpath} at line {line} contains the nix search path expression \"{text}\" which may point outside the directory of that package.",
                         ),
                     NixFileErrorKind::OutsidePathReference =>
-                        write!(
+                        writedoc!(
                             f,
-                            "{relative_package_dir}: File {subpath} at line {line} contains the path expression \"{text}\" which may point outside the directory of that package.",
+                            "
+                            - {relative_package_dir}: File {subpath} at line {line} contains the path expression \"{text}\" which may point outside the directory of that package.
+                              This is undesirable because it creates dependencies between internal paths, making it harder to reorganise Nixpkgs in the future.
+                              Alternatives include:
+                              - If you are creating a new version of a package with a common file between versions, consider following the recommendation in https://github.com/NixOS/nixpkgs/tree/master/pkgs/by-name#recommendation-for-new-packages-with-multiple-versions.
+                              - If the path being referenced could be considered a stable interface with multiple uses, consider exposing it via a `pkgs` attribute, then taking it as a attribute argument in {PACKAGE_NIX_FILENAME}.
+                              - If the path being referenced is internal and has multiple uses, consider passing the file as an explicit `callPackage` argument in `pkgs/top-level/all-packages.nix`.
+                              - If the path being referenced is internal and will need to be modified independently of the original, consider copying it into the {relative_package_dir} directory.
+                            "
                         ),
                     NixFileErrorKind::UnresolvablePathReference { io_error } =>
                         write!(
