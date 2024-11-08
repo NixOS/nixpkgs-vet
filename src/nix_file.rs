bd701464b0df0fc3fb231e91f05c3f40f18c3c32
Will Bush <git@willbush.dev>
2024-11-08 06:06:02 -0600

    Remove unnecessary map_right and to_string

--- src/nix_file.rs
+++ src/nix_file.rs
@@ -563,12 +563,7 @@ mod tests {
             (24, 11, Left("testL")),
         ];
         let expected = BTreeMap::from_iter(cases.map(|(line, column, result)| {
-            (
-                Position { line, column },
-                result
-                    .map(ToString::to_string)
-                    .map_right(|node| node.to_string()),
-            )
+            (Position { line, column }, result.map(ToString::to_string))
         }));
         let actual = BTreeMap::from_iter(cases.map(|(line, column, _)| {
             (
